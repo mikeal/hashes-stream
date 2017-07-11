7b7298e78b3d0da6e12496933039735518bf807c
mikeal <mikeal.rogers@gmail.com>
2017-07-11 16:17:18 -0400

    Adding multi-chunk test.

--- test.js
+++ test.js
@@ -7,6 +7,17 @@ test('basic sha256, one chunk', t => {
     t.error(err)
     t.equal('421c76d77563afa1914846b010bd164f395bd34c2102e5e99e0cb9cf173c1d87', signature)
   })
-  stream.write(new Buffer('asdfasdfasdf'))
+  stream.write(Buffer.from('asdfasdfasdf'))
+  stream.end()
+})
+
+test('basic multi-chunk', t => {
+  t.plan(2)
+  let stream = hashing('sha256', (err, signature) => {
+    t.error(err)
+    t.equal('421c76d77563afa1914846b010bd164f395bd34c2102e5e99e0cb9cf173c1d87', signature)
+  })
+  stream.write(Buffer.from('asdfas'))
+  stream.write(Buffer.from('dfasdf'))
   stream.end()
 })
